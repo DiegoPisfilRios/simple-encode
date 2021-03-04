63bc007fc535f20ae432b89c223b9509f8e94f40
Diego Pisfil <fdiego.prios@gmail.com>
2021-03-03 19:51:19 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementById('encode').onclick = () => {
+    var str = document.getElementById('in').value
+    document.getElementById('out').value = encode(str)
+}
+
+document.getElementById('decode').onclick = () => {
+    var str = document.getElementById('out').value
+    document.getElementById('ini').value = decode(str)
+}
+
+function encode (str) {
+    let cad1 = ''
+    let cad2 = ''
+
+    for(let i = 0; i < str.length; i++) {
+        if((i + 1) % 2 == 0) {
+            cad1 = cad1 + str.charAt(i)
+        }else {
+            cad2 = cad2 + str.charAt(i)
+        }
+    }
+
+    return cad2 + cad1
+}
+
+function decode(str) {
+    let cad1 = str.substring(0, (str.length + 1) / 2)
+    let cad2 = str.substring(cad1.length , str.length)
+
+    let tc = str.length
+    str = ''
+    let c1 = 0
+    let c2 = 0
+    for(let i = 0; i < tc; i++) {
+        if((i % 2) == 0) {
+            str = str +cad1.charAt(c1)
+            c1++
+        }else {
+            str = str + cad2.charAt(c2)
+            c2++
+        }
+    }
+    return str
+}
