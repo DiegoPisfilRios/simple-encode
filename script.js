8fde4be67d0842b6e547f93ec35e38acab486af2
DiegoPisfilRios <77468729+DiegoPisfilRios@users.noreply.github.com>
2023-03-24 16:11:01 +0000

    change ui for desktop

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 document.getElementById('encode').onclick = () => {
-    var str = document.getElementById('in-code').value
-    document.getElementById('out').innerText =  btoa(encode(str));
+  var str = document.getElementById('in-code').value;
+  try {
+    if (str.length > 0) {
+      document.getElementById('out').innerText = btoa(unescape(encodeURIComponent(encode(str))));
+    } else {
+      document.getElementById('out').innerText = '[...]';
+      document.getElementById('out').setAttribute("style", "color: #fff;")
+    }
+  } catch (error) {
+    document.getElementById('out').innerText = '[No se admiten caracteres especiales]';
+    document.getElementById('out').setAttribute("style", "color: orange;")
+  }
 }
 
 document.getElementById('decode').onclick = () => {
-    var str = document.getElementById('in-deco').value
-    document.getElementById('ini').innerText = decode(atob(str));
+  var str = document.getElementById('in-deco').value;
+  try {
+    if (str.length > 0) {
+      document.getElementById('ini').innerText = decode(decodeURIComponent(escape(atob(str))));
+    } else {
+        document.getElementById('ini').innerText = '[...]';
+        document.getElementById('ini').setAttribute("style", "color: #fff;")
+    }
+  } catch (error) {
+    document.getElementById('ini').innerText = '[CADENA CORRUPTA]';
+    document.getElementById('ini').setAttribute("style", "color: orange;")
+  }
 }
 
 function encode (str) {
@@ -33,7 +53,7 @@ function decode(str) {
     let c2 = 0
     for(let i = 0; i < tc; i++) {
         if((i % 2) == 0) {
-            str = str +cad1.charAt(c1)
+            str = str + cad1.charAt(c1)
             c1++
         }else {
             str = str + cad2.charAt(c2)
@@ -44,19 +64,25 @@ function decode(str) {
 }
 
 function onCopy(str) {
-    console.log(str)
-    let text = document.querySelector(`#${str}`).innerText
+    let text = document.querySelector(`#${str}`).innerText;
+
+    if (text == '[...]') {
+      text = 'EnKids 😊'
+    }
+  
+    document.querySelector('#pop-copy').setAttribute('style', 'visibility: visible;')
+  
     navigator.clipboard.writeText(text)
-        .then(() => {
-            console.log('copiiiii')
-            document.querySelector('#pop-copy').classList.add('show')
-            setTimeout(()=>{
-                document.querySelector('#pop-copy').classList.add('hidden')
-            }, 1000)
-           
-        }, (err) => {
-            console.log(err)
-        })
-         document.querySelector('#pop-copy').classList.remove('show');
-            document.querySelector('#pop-copy').classList.remove('hidden');
+      .then(() => {
+        
+        document.querySelector('#pop-copy').classList.add('show');
+        setTimeout(()=>{
+          document.querySelector('#pop-copy').classList.remove('show');
+          setTimeout(() => {
+            document.querySelector('#pop-copy').setAttribute('style', 'visibility: hidden;')
+          }, 500)
+        }, 1000);
+      }, (err) => {
+        console.log('COPY: E-'+err);
+      });
 }
